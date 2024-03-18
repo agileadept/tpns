e32315405a6f26a234a4e3aea8b6f2c5c2db718a
jay <chenzeyu@dasheng.tv>
2024-03-18 17:40:30 +0800

    u

--- src/TagItem.php
+++ src/TagItem.php
@@ -18,11 +18,11 @@ class TagItem
     const XG_AUTO_DEVICEVERSION = 'xg_auto_deviceversion';  // 手机机型，MI 9 SE，vivo X9Plus 等
     const XG_AUTO_COUNTRY = 'xg_auto_country';              // 国家，CN，SG 等
 
-    public $tags;// array of string
-    public $is_not = false;
-    public $tags_operator = "";
-    public $items_operator = "";
-    public $tag_type = "";
+    public $tags;               // array of string，具体标签值，类型：string，如 tag1，guangdong 等。
+    public $is_not = false;     // 是否对 tag 数组的运算结果进行非运算。true：非运算；false：不进行非运算
+    public $tags_operator = ""; // tags 内标签对应的运算符。OR：或运算；AND：且运算
+    public $items_operator = "";// tag_items 数组内各元素的运算符，第一个 tag_items 元素的 items_operator 为无效数据，第二个 tag_items 元素的 items_operator 作为第一个和第二个 tag_items 元素之间的运算符。 OR ：或运算 ;AND ：且运算。注意：不同规则之间运算符逻辑优先级「AND」>「OR」
+    public $tag_type = "";      // 参见  tag_type 取值表
 
     public function filter()
     {
--- src/TagRule.php
+++ src/TagRule.php
@@ -11,9 +11,9 @@ class TagRule
     /**
      * @var TagItem[]
      */
-    public $tag_items;
-    public $is_not = false;
-    public $operator = "";
+    public $tag_items;     // 标签规则，参见 tag_items 说明
+    public $is_not = false;// 是否对 tag_items 数组的运算结果进行非运算。true：进行非运算；false：不进行非运算
+    public $operator = ""; // tag_rules 数组内各元素的运算符，第一个 tag_rules 元素的 operator 为无效数据，第二个 tag_rules 元素的 operator 作为第一个和第二个 tag_rules 元素之间的运算符。OR： 或运算;AND：且运算
 
     public function filter()
     {
